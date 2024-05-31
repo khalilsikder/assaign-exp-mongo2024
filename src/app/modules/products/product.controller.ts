c54d1c7e450c4ba56efa170cec0478a2fdedede2
khalilsikder <khalil.sikder03@gmail.com>
2024-05-31 12:18:44 +0600

    added zod

--- src/app/modules/products/product.controller.ts
+++ src/app/modules/products/product.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import { ProductServices } from "./product.service";
+import ProductValidationSchema from "./product.validation";
 
 const createProduct = async (req: Request, res: Response) => {
   try {
     const { product: productData } = req.body;
-
-    const result = await ProductServices.createProductBD(productData);
+    const zodparseData = ProductValidationSchema.parse(productData)
+    const result = await ProductServices.createProductBD(zodparseData);
     res.status(200).json({
       success: true,
       message: "product is create successfull",
